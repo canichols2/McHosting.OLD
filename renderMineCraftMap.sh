30fc44fcbb7ee3b650ecc514cf5ad8454724d2f6
Shackelford-Arden <ardenshackelford@gmail.com>
2017-02-06 22:26:17 -0600

    Made changes to all scripts

--- renderMineCraftMap.sh
+++ renderMineCraftMap.sh
@@ -4,37 +4,41 @@
 # Due to limitation of memory on the VPS with AWS, run this script during times where no one is on the server.
 # The amount of memory needed for the render kills the MineCraft server.
 
-echo "Starting Render..."
+###############################################################################################
+
+# echo "Starting Render..."
 
 # Like a smart person, we make a backup first.
-echo "Creating a backup of the data"
-echo "This is the data that we will use for the render just in case something goes wrong"
+# echo "Creating a backup of the data"
+# This is the data that we will use for the render just in case something goes wrong
 
-/home/ubuntu/minecraft/backupMinecraftServer.sh
+# /home/ubuntu/minecraft/backupMinecraftServer.sh
+
+
+################################################################################################
+################################################################################################
+
 
 # Begin render process.
 
 # Create a date variable to make sure that we create a render using the most recent data.
 
 latestFile="$(ls -t /home/ubuntu/minecraftBackups | head -n1)"
+
 echo "Moving to the backups folder"
 tmux send -t minecraftServer 'cd /home/ubuntu/minecraftBackups' ENTER
 
 echo "Delete the previous render's extract"
-tmux send -t minecraftServer 'rm -rf ./home' ENTER
+tmux send -t minecraftServer 'rm -rf /home/ubuntu/minecraftBackups/home' ENTER
 
 echo "Uncompress the backup we made a moment ago"
 tmux send -t minecraftServer 'tar -xf '$latestFile ENTER
 
 echo "Move into the correct directory to begin the render"
-tmux send -t minecraftServer 'cd ./home/ubuntu/minecraft/' ENTER
+tmux send -t minecraftServer 'cd /home/ubuntu/minecraftBackups/home/ubuntu/minecraft/' ENTER
 
 echo "Begin the render process!"
 tmux send -t minecraftServer 'overviewer.py --rendermodes=smooth-lighting ./MCMadness /var/www/minecraft' ENTER
-
-echo "Now we'll wait 25 minutes for the render to complete"
-
-# sleep 25m
 
 #echo "Render complete! Now copying files to web server"
 #tmux send -t minecraftServer 'cp -R /home/ubuntu/mcOverview/* /var/www/minecraft/' ENTER
@@ -48,10 +52,8 @@ tmux send -t minecraftServer 'cp /home/ubuntu/minecraft/index.html.bak /var/www/
 echo "Make sure the .htaccess file is present to ensure better security"
 tmux send -t minecraftServer 'cp /home/ubuntu/minecraft/.htaccess /var/www/minecraft/.htaccess' ENTER
 
-#echo "Cleaning the mcOverview directory for future use..."
-#tmux send -t minecraftServer 'rm -rf /home/ubuntu/mcOverview/*' ENTER
+# echo "Cleaning the mcOverview directory for future use..."
+# tmux send -t minecraftServer 'rm -rf /home/ubuntu/mcOverview/*' ENTER
 
 echo "Cleaning up uncompressed backup..."
 tmux send -t minecraftServer 'rm -rf /home/ubuntu/minecraftBackups/home' ENTER
-
-echo "Render process complete! Go to http://minecraft.ardenshackelford.com to view it!"
--- stopMinecraftServer.sh
+++ stopMinecraftServer.sh
@@ -7,12 +7,3 @@ echo "Stopping server..."
 # Let's stop the Server
 
 tmux send -t minecraftServer '/stop' ENTER
-
-echo "Wait a minute while the server ends..."
-
-# Use NodeJS for the rest of this script
-#wait 120
-
-# echo "Ok, the server has stopped"
-
-
