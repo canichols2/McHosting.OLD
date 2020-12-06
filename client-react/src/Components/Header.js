f56f1c14b5131b840bc7fefeee2f83ede1b21d17
Cornelison-Cody <codysmart@gmail.com>
2020-12-06 01:05:43 -0800

    Theme colors, and header started

---
+++ client-react/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+import Titlebar from './Titlebar';
+
+export default function Header() {
+
+   return (
+      <>
+         <Titlebar />
+         <Navbar />
+      </>
+   )
+}
--- client-react/src/index.js
+++ client-react/src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#66BB6A',
+      main: '#64CC57',
+      dark: '#43A047',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#8d6e63',
+      main: '#795548',
+      dark: '#5d4037',
+      contrastText: '#000',
+    },
+  },
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
